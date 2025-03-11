e4d55f1f4b233186c75e13d35756e3b75eb828ac
HammerMeow <ps.shishkov1@gmail.com>
2025-03-11 22:52:32 +0300

    - развил представление о документе

--- data_obj.py
+++ data_obj.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Union
 import logging
 
 import json
@@ -15,7 +16,7 @@ def load_paper(path):
     try:
         with open(path2json, 'r') as f:
             content_saved = json.load(f)
-        obj = BasePaper(**content_saved)
+        obj = Paper(**content_saved)
     except FileNotFoundError:
         logging.warning("File not found: " + path)
         obj = None
@@ -23,16 +24,47 @@ def load_paper(path):
     return obj
 
 
-class BasePaper(BaseModel):
+class PaperDocPiece(BaseModel):
+    pass
+
+
+class PaperDocEquation(BaseModel):
+    pass
+
+
+class PaperDocPicture(BaseModel):
+    pass
+
+
+class PaperDocTable(BaseModel):
+    pass
+
+
+class PaperDocReference(BaseModel):
+    pass
+
+
+class PaperDoc(BaseModel):
+    text: List[PaperDocPiece]
+    equations: List[PaperDocEquation]
+    pictures: List[PaperDocPicture]
+    tables: List[PaperDocTable]
+    bibliography: List[PaperDocReference]
+
+    graph: List[dict[str, int]]
+
+
+class Paper(BaseModel):
     title: str
-    abstract: str
+    title_id: str
+    summary: str
+
     link_paper: str
-    link_github: str = ''
     version: int
 
-    doc: dict
+    doc: PaperDoc
 
-    vec: list
+    vec: List[Union[int, float]]
     index: int
 
 
@@ -41,7 +73,8 @@ if __name__ == "__main__":
 
     content = {
         "title": "Flattened Convolutional Neural Networks For Feedforward Acceleration",
-        "abstract": "We present flattened convolutional neural networks that are designed for fast feedforward execution. The redundancy of the parameters, especially weights of the convolutional filters in convolutional neural networks has been extensively studied and different heuristics have been proposed to construct a low rank basis of the filters after training. In this work, we train flattened networks that consist of consecutive sequence of one-dimensional filters across all directions in 3D space to obtain comparable performance as conventional convolutional networks. We tested flattened model on different datasets and found that the flattened layer can effectively substitute for the 3D filters without loss of accuracy. The flattened convolution pipelines provide around two times speed-up during feedforward pass compared to the baseline model due to the significant reduction of learning parameters. Furthermore, the proposed method does not require efforts in manual tuning or post processing once the model is trained.",
+        "title_id": "2110.00476",
+        "summary": "We present flattened convolutional neural networks that are designed for fast feedforward execution. The redundancy of the parameters, especially weights of the convolutional filters in convolutional neural networks has been extensively studied and different heuristics have been proposed to construct a low rank basis of the filters after training. In this work, we train flattened networks that consist of consecutive sequence of one-dimensional filters across all directions in 3D space to obtain comparable performance as conventional convolutional networks. We tested flattened model on different datasets and found that the flattened layer can effectively substitute for the 3D filters without loss of accuracy. The flattened convolution pipelines provide around two times speed-up during feedforward pass compared to the baseline model due to the significant reduction of learning parameters. Furthermore, the proposed method does not require efforts in manual tuning or post processing once the model is trained.",
         "link_paper": "https://arxiv.org/abs/1412.5474",
         "link_github": "",
         "version": 4,
@@ -53,7 +86,7 @@ if __name__ == "__main__":
     obj = load_paper(path2json)
 
     if obj is None:
-        obj = BasePaper(**content)
+        obj = Paper(**content)
 
     print(obj.title)
     obj_json = obj.model_dump_json()
