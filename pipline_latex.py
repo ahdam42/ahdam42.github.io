333737ee5eb418ceb3b2c564d9c089cc80f712df
HammerMeow <ps.shishkov1@gmail.com>
2025-03-12 23:25:31 +0300

    - I'm going to sleep

--- pipline_latex.py
+++ pipline_latex.py
@@ -40,11 +40,22 @@ def lock_search(func):
     return wrapped
 
 
-def get_document(nodes: list) -> Union[None, pylxt.LatexEnvironmentNode]:
+def get_env(nodes, env_name: str) -> Union[None, pylxt.LatexEnvironmentNode]:
+    if type(nodes) is not list:
+        nodes = [nodes]
+
     for nd in nodes:
         if type(nd) is pylxt.LatexEnvironmentNode:
-            return nd
+            nd: pylxt.LatexEnvironmentNode
+
+            if nd.environmentname == env_name:
+                return nd
     return None
+
+
+def get_document(nodes: list) -> Union[None, pylxt.LatexEnvironmentNode]:
+    return get_env(nodes, 'document')
+
 
 @lock_search
 def get_title(node, reset_lock_search=False) -> Union[None, str]:
@@ -80,6 +91,25 @@ def get_authors(node, reset_lock_search=False) -> Union[None, str]:
         return None
 
 
+@lock_search
+def get_abstract(node, reset_lock_search=False) -> Union[None, str]:
+    env = get_env(node, 'abstract')
+
+    if env is None:
+        return None
+
+    txt = ''
+    for nd in env.nodelist:
+        if type(nd) is pylxt.LatexCharsNode:
+            nd: pylxt.LatexCharsNode
+
+            txt += nd.chars
+        else:
+            print(nd)
+
+    return txt
+
+
 def simple_parser(latex_nodes: list) -> DataObj.Paper:
     env = get_document(paper_nodes)
 
@@ -88,6 +118,9 @@ def simple_parser(latex_nodes: list) -> DataObj.Paper:
     for nd in env.nodelist:
         paper_obj.title = get_title(nd)
         paper_obj.authors = get_authors(nd)
+        abst = get_abstract(nd)
+
+    print(abst)
 
     get_title(None, reset_lock_search=True)
     get_authors(None, reset_lock_search=True)
@@ -96,15 +129,11 @@ def simple_parser(latex_nodes: list) -> DataObj.Paper:
 
 
 if __name__ == '__main__':
-
     path2latex = 'arx.tex'
     with open(path2latex, 'r') as f:
         latex_txt = f.read()
 
-    # Создание экземпляра LatexWalker
     walker = pylxt.LatexWalker(latex_txt)
-
-    # Парсинг LaTeX-кода
     paper_nodes, _, _ = walker.get_latex_nodes()
 
     paper = simple_parser(paper_nodes)
